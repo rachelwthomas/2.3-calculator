d52161fdf7fd80ce6991c8a05e1493d5d0a527a3
rachelwthomas <58479626+rachelwthomas@users.noreply.github.com>
2020-01-16 16:52:29 -0500

    for loop for pushNumber working

--- main.js
+++ main.js
@@ -1,24 +1,34 @@
+(function() {
+  'use strict';
+
+
+
 // => pushNumber() {
 //   document.querySelector('.key').addEventListener("click",alert());
 // }
 // document.querySelector('button').addEventListener,("click",pushNumber());
 
 // document.querySelector(".button").addEventListener("click", pushNumber);
-(function() {
-  'use strict';
-
-
-function pushNumber() {
 
 let numbers = document.querySelectorAll('.number');
 
+
 for (let i=0; i < numbers.length; i=i+1) {
+  console.log(numbers[i]);
   numbers[i].addEventListener("click", pushNumber);
-}
-
-alert("you pushed a button");
 
 }
+
+function pushNumber() {
+  alert("you pushed a button");
+}
+
+
+
+
+
+
+
 // function pushNumber() {
 //   alert("you pushed a button");
 //
