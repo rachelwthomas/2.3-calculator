0b212ada2a885044872bb5b1feffeb8b28235ee0
rachelwthomas <58479626+rachelwthomas@users.noreply.github.com>
2020-01-16 15:35:34 -0500

    only the first button works gives an alert when pushed

---
+++ main.js
@@ -0,0 +1,11 @@
+// => pushNumber() {
+//   document.querySelector('.key').addEventListener("click",alert());
+// }
+// document.querySelector('button').addEventListener,("click",pushNumber());
+
+document.querySelector(".button").addEventListener("click", pushNumber);
+
+function pushNumber() {
+  alert("you pushed a button");
+
+}
