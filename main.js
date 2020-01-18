08ca9ee4165ce77b2e2feb0be784efc11ffbcf7e
rachelwthomas <58479626+rachelwthomas@users.noreply.github.com>
2020-01-17 21:17:07 -0500

    pushNumber function returns a number instead of a string

--- main.js
+++ main.js
@@ -12,202 +12,46 @@
 
 
 //**************************************************** PUSH NUMBER FUNCTION
-// returns only the value of seven no matter what number is pushed
-
 // let numbers = document.querySelectorAll('.number');
 //
 //
+//
+// let pushNumber = (event) => {
+// console.log(`e ${event.target.value}`);
+//
+// }
+//
 // for (let i=0; i < numbers.length; i=i+1) {
-//   console.log(numbers[i]);
-//   numbers[i].addEventListener("click", pushNumber);
-//
+// numbers[i].addEventListener('click', pushNumber);
 // }
-//
-// function pushNumber() {
-//   let x = document.querySelector('.number').value;
-//   alert(x);
-// }
+// ******************pushNumber fucntion returns a value in the console..o
+
+
+
+
+
 // *********************************************************************
-// let numbers = document.querySelectorAll('.number');
-//
-//
-// for (let i=0; i < numbers.length; i=i+1) {
-//   console.log(numbers[i]);
-//   numbers[i].addEventListener("click", pushNumber);
-//
-//
-// }
 
-
-// function pushNumber() {
-//   let numbers = document.querySelector('.number').value;
-//   console.log(numbers.value);
-//
-//   for (let i=0; i < numbers.length; i=i+1) {
-//     // console.log(numbers[i]);
-//     numbers[i].addEventListener("click", pushNumber);
-//     // console.log(numbers[i]);
-//
-//   alert();
-// }}
-
-
-// let numbers = document.querySelectorAll('.number');
-//
-//
-// for (let i=0; i < numbers.length; i=i+1) {
-//   console.log(numbers[i]);
-//   numbers[i].addEventListener("click", pushNumber);
-//   // alert(numbers[i].value);
-//
-// }
-
-// function pushNumber() {
-//   let numbers = document.querySelectorAll('.number');
-//
-//
-//   for (let i=0; i < numbers.length; i=i+1) {
-//     console.log(numbers[i]);
-//     numbers[i].addEventListener("click", pushNumber);
-//     alert(numbers[i].value);
-//
-//   }
-//
-// }
-
-
-
-
-//
-//
-// let numbers = document.querySelectorAll('.number');
-//
-//
-// for (let i=0; i < numbers.length; i=i+1) {
-//   console.log(numbers[i]);
-//   numbers[i].addEventListener("click", pushNumber);
-//
-// }
-//
-// function pushNumber() {
-//   let numbers = document.querySelector('.number').value;
-//   alert(numbers[0].value);
-// }
-
-// function pushNumber() {
-//   let x = document.querySelector('.number').value;
-//   // alert(numbers[1].value);
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-// let numbers = document.querySelectorAll('.number');
-//
-//
-// for (let i=0; i < numbers.length; i=i+1) {
-//   console.log(numbers[i]);
-//   numbers[i].addEventListener("click", pushNumber);
-//
-// }
-
-//   let value = document.querySelectorAll('.number');
-//
-//   for(let i=0; i < value.length;i=i+1){
-//   console.log(value[i]);
-//   value[i].addEventListener("click", pushNumber);
-// }
-//
-//   function pushNumber() {
-//     alert(value.value);
-//   // let value = document.querySelectorAll('.number').value;
-//
-//     // for (let i=0; i < value.length; i=i+1){}
-//     // console.log(value[i]);
-//   }
-  // alert(value);
- //
- //  let numbers = document.querySelectorAll('.number');
- //
- //
- // for (let i=0; i < numbers.length; i=i+1) {
- //   console.log(numbers[i]);
- //   numbers[0].addEventListener("click", pushNumber);
- //
- // }
- //
- // for (let i=0; i < numbers.length; i=i+1) {
- //   console.log(numbers[i]);
- //   numbers[1].addEventListener("click", pushNumber);
- //
- //
- // }
- //
- // function pushNumber() {
- //   // let x = document.querySelector('.number').value;
- // //   alert(numbers());
- // }
-
-
- // let numbers = document.querySelectorAll('.number');
- //
- //
- // for (let i=0; i < numbers.length; i=i+1) {
- //   console.log(numbers[i]);
- //   numbers[i].addEventListener("click", pushNumber);
- //
- // }
- //
- // function pushNumber() {
- //   let numbers = document.querySelectorAll('.number');
- //   alert(numbers);
- // }
-
-
-
-//  let numbers = document.querySelectorAll('.number');
-//
-//
-//  for (let i=0; i < numbers.length; i=i+1) {
-//    console.log(numbers[i]);
-//    numbers[i].addEventListener("click", pushNumber);
-//
-//  }
-//
-//  function pushNumber() {
-//    console.log('this', this);
-//    // let x = document.querySelector('.number').value;
-//    // alert(x);
-//  }
-//
-//
-// $buttons[i].addEventListener('click')
-
-// **********************************************************
 
  let numbers = document.querySelectorAll('.number');
 
 
 
  let pushNumber = (event) => {
- console.log(`e ${event.target.value}`);
+ let value = (`${event.target.value}`);
+
+console.log(Number.isInteger(Number(value)));
+
+
 
  }
 
  for (let i=0; i < numbers.length; i=i+1) {
  numbers[i].addEventListener('click', pushNumber);
+
+
+
  }
-
-
-
 
 
 
@@ -216,23 +60,6 @@
 //  }
 //
 //
-//
-//
-//
-//  function pushNumber() {
-//    console.log('this', this);
-//    // let x = document.querySelector('.number').value;
-//    // alert(x);
-//  }
-//
-//
-// $buttons[i].addEventListener('click')
-
-
-
-// target.value
-
-
 
 
 // ****************************************************PUSH OPERATOR FUNCTION
@@ -240,18 +67,17 @@
 let operators = document.querySelectorAll('.operator');
 
 
+
+
+function pushOperator() {
+ console.log(`${event.target.value}`);
+}
+
+
 for (let i=0; i < operators.length; i=i+1) {
-  console.log(operators[i]);
   operators[i].addEventListener("click", pushOperator);
 
 }
-
-function pushOperator() {
-  alert("you pushed an operator");
-}
-
-
-
 
 
 
